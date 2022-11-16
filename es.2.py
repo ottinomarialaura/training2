f8f6566ae516c3163862f9af5680a82a46849a45
ottinomarialaura <ottino.marialaura@iisgalvanimi.edu.it>
2022-11-16 11:58:59 +0000

    es.2.py

--- es.2.py
+++ es.2.py
@@ -13,4 +13,26 @@ funzione1("rosso")
 funzione1("nero")
 funzione1("tricolore")
 
-#
+#una funzione deve essere chiamata con il numero corretto di argomenti, quindi bisogna chiamare la funzione con il numero di argomenti, non di più e non di meno
+def my_function(nome, cognome, età):
+  print(nome + " " + cognome + " " + età)
+
+my_function("laura", "ottino,", "18 anni")
+
+#nel caso non conoscessi il contenuto della funzione e si voglia stamparlo comunque, basta aggiungere *davanti al nome della funzione
+def my_function(*informazioni):
+  print(*informazioni)
+
+my_function("laura", "ottino", "18 anni", "vive a Milano")
+
+#nel momento in cui gli argomenti nella funzione sono in disordine e li si vuole stampare ordinatamente, allora dobbiamo definire ogni nome a ogni argomento 
+def my_function(bimbo1, bimbo3, bimbo4, bimbo2):
+  print(bimbo1, bimbo2, bimbo3, bimbo4)
+
+my_function(bimbo1 = "Emil", bimbo2 = "Tobias", bimbo3 = "Linus", bimbo4 = "Charlie")
+
+#quando gli argomenti nella funzione sono in disordine e si vuole stampare un determinato nome in una determinata posizione bisogna usare **
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
+
+my_function(fname = "Tobias", lname = "Refsnes")
